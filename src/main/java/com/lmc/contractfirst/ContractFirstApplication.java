c267679eb1cff6fe11dfb3b2e6c70d3108809ceb
Leticia Corral <leticiacorral@Leticias-MacBook-Air.local>
2022-01-19 14:52:36 -0300

    Criando classes de representaçao a partir de um swagger

---
+++ src/main/java/com/lmc/contractfirst/ContractFirstApplication.java
@@ -0,0 +1,13 @@
+package com.lmc.contractfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lmc/contractfirst/ContractFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmc.contractfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
